b6cbdd6424633152199634469c78938862107b79
Andrey Sergeev <SergeevAndreyAleksandrovich@yandex.ru>
2025-11-28 11:02:44 +0300

    Exercise 7.3

--- homework.js
+++ homework.js
@@ -27,3 +27,19 @@ function calculate (a, b, operator) {
 }
 const result = calculate.apply(calculate, [2, 3, "+"]);
 console.log(result);
+
+//Задание 7.3
+const arr = [
+    {name: "Антон", age: 25},
+    {name: "Алёна", age: 20},
+    {name: "Эдуард", age: 45},
+]
+const userAge = arr.filter((arr) => {
+    return arr.age > 18;
+})
+console.log(userAge)
+const newArr = [];
+for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i].name);
+}
+console.log(newArr)
