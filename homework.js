b9783e8aef791809a6090ada40a2157df5ee8e10
Andrey Sergeev <SergeevAndreyAleksandrovich@yandex.ru>
2025-11-28 10:21:38 +0300

    Exercise 7.2

--- homework.js
+++ homework.js
@@ -9,3 +9,21 @@ const person = {
 
 const res = printInfo.call(person, person.name, person.age);
 res;
+
+//Задание 7.2
+function calculate (a, b, operator) {
+    if (operator === `+`) {
+        return a + b;
+    }
+    if (operator === `-`) {
+        return a - b;
+    }
+    if (operator === `*`) {
+        return a * b;
+    }
+    if (operator === `/`) {
+        return a / b;
+    }
+}
+const result = calculate.apply(calculate, [2, 3, "+"]);
+console.log(result);
